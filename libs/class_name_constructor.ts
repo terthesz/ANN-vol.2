9664ccd4ce70dadfaf36364187607316e6fe5969
terthesz <60745997+terthesz@users.noreply.github.com>
2022-10-09 18:51:36 +0200

    ✨ disabled calling of class name constructor

--- libs/class_name_constructor.ts
+++ libs/class_name_constructor.ts
@@ -7,7 +7,16 @@ function ClassNameConstructor<T extends { new (...args: any[]): {} }>(
 
       const class_name_constructor = (this as any)[constructor.name];
 
-      if (class_name_constructor) class_name_constructor(...args);
+      if (!class_name_constructor) return;
+
+      class_name_constructor(...args);
+
+      delete (this as any)[constructor.name];
+      (this as any)[constructor.name] = () => {
+        throw new Error(
+          `❗ This function is being used as the class constructor. It is not callable. (${constructor.name}.${constructor.name})`
+        );
+      };
     }
   }
 
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-import Network from "./network";
+import Network from './network';
 
 new Network();
